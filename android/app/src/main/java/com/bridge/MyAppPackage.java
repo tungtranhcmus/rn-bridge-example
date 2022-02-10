3683221be123fb34fe32f296ad5366185f5df42a
Tung Tran <tungtran.hcmus@gmail.com>
2022-02-10 13:35:02 +0700

    Update

--- android/app/src/main/java/com/bridge/MyAppPackage.java
+++ android/app/src/main/java/com/bridge/MyAppPackage.java
@@ -22,6 +22,7 @@ public class MyAppPackage implements ReactPackage {
         List<NativeModule> modules = new ArrayList<>();
 
         modules.add(new CalendarModule(reactContext));
+        modules.add(new Bulb(reactContext));
 
         return modules;
     }
